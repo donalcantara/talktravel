f4d81d8c1f50d5910763e583499a0c1ea2f9985c
Don Alcantara <don.alcantara@yahoo.com>
2016-10-18 20:45:51 -0700

    heroku changes

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, request, redirect, render_template, session, flash
 from mysqlconnection import MySQLConnector
 from time import time
@@ -186,7 +188,11 @@ def update(user_id):
 	mysql.query_db(query, data)
 	return redirect('/wall')
 
-app.run(debug=True)
+if __name__ == "__main__":
+   port = int(os.environ.get("PORT", 5000))
+   app.run(host='0.0.0.0', port=port)
+
+# app.run(debug=True)
 
 
 
